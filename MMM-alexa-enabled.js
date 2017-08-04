5544ee12fbc2524e07d0f58a9a73024c9fc4b589
Eric Burlingame <eric@burlingames.com>
2017-08-03 18:09:41 -0700

    Fixed fade time not working (again)

--- MMM-alexa-enabled.js
+++ MMM-alexa-enabled.js
@@ -15,24 +15,24 @@ Module.register("MMM-alexa-enabled", {
 		var self = this;
         if (message['type'] == "ActivateModule") {
          	MM.getModules().withClass(message['module_name']).exceptModule(this).enumerate(function(module) {
-                module.show(self.config.fadeTime);
+                module.show(self.config.fade_time);
             });
         }
         else if (message['type'] == "DeactivateModule") {
         	console.log("hiding");
         	MM.getModules().withClass(message['module_name']).exceptModule(this).enumerate(function(module) {
         		console.log("Hiding " + module.name);
-                module.hide(self.config.fadeTime);
+                module.hide(self.config.fade_time);
             });
         }
         else if (message['type'] == "ActivateAllModules") {
             MM.getModules().exceptModule(this).enumerate(function(module) {
-                module.show(self.config.fadeTime);
+                module.show(self.config.fade_time);
             });
         }
         else if (message['type'] == "DeactivateAllModules") {
             MM.getModules().exceptModule(this).enumerate(function(module) {
-                module.hide(self.config.fadeTime);
+                module.hide(self.config.fade_time);
             });
         }
 	},
